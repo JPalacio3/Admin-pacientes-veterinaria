c9e0c3fa08a0da68f517b1cbf8a741228f84dd0e
JPalacio3 <joelpalacio630@gmail.com>
2025-05-03 17:30:11 -0600

    Añadir botón "Eliminar" en PatientDetails y ajustar estilos en PatientForm

--- src/components/PatientDetail.tsx
+++ src/components/PatientDetail.tsx
@@ -9,7 +9,7 @@ export default function PatientDetails({ patient }: PatientDetailProps) {
   return (
     <div
       className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl
-    hover:ease-in-out hover:duration-300 hover:scale-105 cursor-pointer"
+    cursor-pointer"
     >
       <PatientDetailItem label="ID" data={patient.id} />
       <PatientDetailItem label="Nombre" data={patient.name} />
@@ -17,6 +17,24 @@ export default function PatientDetails({ patient }: PatientDetailProps) {
       <PatientDetailItem label="Email" data={patient.email} />
       <PatientDetailItem label="Fecha de alta" data={patient.date.toString()} />
       <PatientDetailItem label="Sintomas" data={patient.symptoms} />
+
+      <div className="flex justify-between mt-10 ">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700
+         text-white font-bold uppercase rounded-lg "
+        >
+          Editar
+        </button>
+
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700
+         text-white font-bold uppercase rounded-lg "
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -10,10 +10,12 @@ export default function PatientForm() {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<DraftPatient>();
 
   const registerPatient = (data: DraftPatient) => {
     addPatient(data);
+    reset();
   };
 
   return (
@@ -124,7 +126,7 @@ export default function PatientForm() {
         <input
           type="submit"
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold
-          hover:bg-indigo-700 cursor-pointer transition-colors"
+          hover:bg-indigo-700 cursor-pointer transition-colors rounded-lg"
           value="Guardar Paciente"
         />
       </form>
