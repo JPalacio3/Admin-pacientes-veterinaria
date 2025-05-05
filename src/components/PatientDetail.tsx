08af89b53a6cd965c528fc88dc1b200fcb86e78e
JPalacio3 <joelpalacio630@gmail.com>
2025-05-05 09:45:18 -0600

    Añadir funcionalidad para eliminar pacientes en PatientDetails y actualizar el store

--- src/components/PatientDetail.tsx
+++ src/components/PatientDetail.tsx
@@ -1,11 +1,14 @@
 import { Patient } from "../types";
 import PatientDetailItem from "./PatientDetailItem";
+import { usePatientSore } from "../store";
 
 type PatientDetailProps = {
   patient: Patient;
 };
 
 export default function PatientDetails({ patient }: PatientDetailProps) {
+  const deletePatient = usePatientSore((state) => state.deletePatient);
+
   return (
     <div
       className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl
@@ -31,6 +34,9 @@ export default function PatientDetails({ patient }: PatientDetailProps) {
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700
          text-white font-bold uppercase rounded-lg "
+          onClick={() => {
+            deletePatient(patient.id);
+          }}
         >
           Eliminar
         </button>
--- src/store.ts
+++ src/store.ts
@@ -2,11 +2,15 @@ import { create } from "zustand";
 import { v4 as uuidv4 } from "uuid";
 import { DraftPatient, Patient } from "./types";
 
+// Definición del estado de los pacientes
 type PatientState = {
   patients: Patient[];
+  activeId: Patient["id"];
   addPatient: (data: DraftPatient) => void;
+  deletePatient: (id: Patient["id"]) => void;
 };
 
+// función para crear un nuevo paciente con un id único
 const createPatient = (patient: DraftPatient): Patient => {
   return {
     ...patient,
@@ -14,12 +18,23 @@ const createPatient = (patient: DraftPatient): Patient => {
   };
 };
 
+// creación del store para las acciones de los pacientes
 export const usePatientSore = create<PatientState>((set) => ({
   patients: [],
+  activeId: "",
+
+  // función para agregar un nuevo paciente
   addPatient: (data) => {
     const newPatient = createPatient(data);
     set((state) => ({
       patients: [...state.patients, newPatient],
+    }));
+  },
+
+  // función para eliminar un paciente
+  deletePatient: (id) => {
+    set((state) => ({
+      patients: state.patients.filter((patient) => patient.id !== id),
     }));
   },
 }));
